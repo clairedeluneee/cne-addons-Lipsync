014c1bf9e703aba6f0faa804ae67499ccf6c8aec
Claire Vivian de Lunette <whotookclaireee@gmail.com>
2026-06-25 21:36:21 +0800

    parseFromSong fix make the damn thing lowercase

--- source/LyricHandler.hx
+++ source/LyricHandler.hx
@@ -4,120 +4,121 @@ import sys.io.File;
 import haxe.Exception;
 import flixel.util.FlxSignal.FlxTypedSignal;
 import flixel.FlxBasic;
-
 import funkin.backend.assets.ModsFolder;
-
 import source.Signal;
 
 /**
-    The object used to handle lyrics.
+	The object used to handle lyrics.
 **/
 class LyricHandler extends FlxBasic {
+	/**
+	 * The `SequencedLine`s for this `LyricHandler`.
+	 */
+	public var sequence:Array<SequencedLine> = [];
 
-    /**
-     * The `SequencedLine`s for this `LyricHandler`.
-     */
-    public var sequence:Array<SequencedLine> = [];
+	/**
+	 * `SequencedLine`s that are have not been played yet.
+	 */
+	public var unplayedLines:Array<SequencedLine> = [];
 
-    /**
-     * `SequencedLine`s that are have not been played yet.
-     */
-    public var unplayedLines:Array<SequencedLine> = [];
+	/**
+	 * `SequencedLine`s that have already been played.
+	 */
+	public var elapsedLines:Array<SequencedLine> = [];
 
-    /**
-     * `SequencedLine`s that have already been played.
-     */
-    public var elapsedLines:Array<SequencedLine> = [];
+	/**
+	 * Dynamic that fires if the next line is played.
+	 */
+	public var onLyricShow:Dynamic<SequencedLine->Void>;
 
-    /**
-     * Dynamic that fires if the next line is played.
-     */
-    public var onLyricShow:Dynamic<SequencedLine -> Void>;
+	/**
+	 * Number of lines played so far.
+	 */
+	public var linesPlayed:Int = 0;
 
-    /**
-     * Number of lines played so far.
-     */
-    public var linesPlayed:Int = 0;
+	/**
+	 * Map that shows ID tags. Useful for metadata.
+	 */
+	public var extras:Map<String, String> = [];
 
-    /**
-     * Map that shows ID tags. Useful for metadata.
-     */
-    public var extras:Map<String, String> = [];
+	/**
+	 * Determines how much to offset the lyrics for synchronization in milliseconds
+	 *
+	 * To apply it, add `[offset:n]` in your lyric file, or alternatively, set this variable directly.
+	 */
+	public var offset:Float = 0;
 
-    /**
-     * Determines how much to offset the lyrics for synchronization in milliseconds 
-     * 
-     * To apply it, add `[offset:n]` in your lyric file, or alternatively, set this variable directly.
-     */
-    public var offset:Float = 0;
+	public function new() {
+		super();
+		onLyricShow = new Signal();
+	}
 
-    public function new(){
-        super();
-        onLyricShow = new Signal();
-    }
+	/**
+	 * Parses a string and turns it into `SequencedLine`.
+	 * @param string
+	 */
+	public function parseString(string:String):Void {
+		var candidates:Array<String> = string.split("\n");
 
-    /**
-     * Parses a string and turns it into `SequencedLine`.
-     * @param string 
-     */
-    public function parseString(string:String):Void {
-        var candidates:Array<String> = string.split("\n");
+		for (line in candidates) {
+			line = StringTools.trim(line);
+			var inBrackets:String = line.substring(line.indexOf("["), line.indexOf("]"));
 
-        for (line in candidates) {
-            line = StringTools.trim(line);
-            var inBrackets:String = line.substring(line.indexOf("["), line.indexOf("]"));
+			var timestamp:Float = SequencedLine.convertFromFormattedTime(inBrackets);
 
-            var timestamp:Float = SequencedLine.convertFromFormattedTime(inBrackets);
+			if (timestamp == -1)
+				continue;
+			if (Math.isNaN(timestamp)) {
+				if (line == "")
+					continue;
+				var split = [
+					inBrackets.substring(1, inBrackets.indexOf(":")),
+					inBrackets.substring(inBrackets.indexOf(":") + 1)
+				];
+				if (split[0] == "#")
+					continue;
+				extras.set(split[0], split[1]);
+				trace(split[0] + ": " + split[1]);
 
-            if (timestamp == -1) continue;
-            if (Math.isNaN(timestamp)) {
-                if (line == "") continue;
-                var split = [
-                    inBrackets.substring(1, inBrackets.indexOf(":")),
-                    inBrackets.substring(inBrackets.indexOf(":") + 1)
-                ];
-                if (split[0] == "#") continue;
-                extras.set(split[0], split[1]);
-                trace(split[0] + ": " + split[1]);
+				if (split[0] == "offset")
+					offset = Std.parseFloat(split[1]);
+				continue;
+			}
 
-                if (split[0] == "offset") offset = Std.parseFloat(split[1]);
-                continue;
-            }
+			var seq:SequencedLine = new SequencedLine(timestamp, line.substring(line.indexOf("]") + 1));
 
-            var seq:SequencedLine = new SequencedLine(timestamp, line.substring(line.indexOf("]") + 1));
+			sequence.push(seq);
+			unplayedLines.push(seq);
+		}
+	}
 
-            sequence.push(seq);
-            unplayedLines.push(seq);
-        }
-    }
+	/**
+	 * Parses the `.lrc` file in `/songs/[song]/[difficulty].lrc`.
+	 *
+	 * @param song The song.
+	 * @param difficulty The difficulty.
+	 */
+	public function parseFromSong(song:String, difficulty:String):Void {
+		song = song.toLowerCase();
+		difficulty = difficulty.toLowerCase();
 
-    /**
-     * Parses the `.lrc` file in `/songs/[song]/[difficulty].lrc`.
-     * 
-     * @param song The song.
-     * @param difficulty The difficulty.
-     */
-    public function parseFromSong(song:String, difficulty:String):Void {
-        try {
-            var fileContent:String = File.getContent("./mods/" + ModsFolder.currentModFolder + '/songs/$song/$difficulty.lrc');
-            parseString(fileContent);
-        } catch (e:Exception) {
-            trace('Can\'t find $difficulty.lrc for song $song, does it exist?');
-            trace("Exception: " + e);
-        }
-    }
+		try {
+			var fileContent:String = File.getContent("./mods/" + ModsFolder.currentModFolder + '/songs/$song/$difficulty.lrc');
+			parseString(fileContent);
+		} catch (e:Exception) {
+			trace('Can\'t find $difficulty.lrc for song $song, does it exist?');
+			trace("Exception: " + e);
+		}
+	}
 
-    override public function update(elapsed:Float):Void {
-
-        for (i in sequence) {
-            if ((i.timestamp + offset) < Conductor.songPosition && !i.played) {
-                i.played = true;
-                onLyricShow.dispatch(i);
-                elapsedLines.push(unplayedLines.shift());
-                linesPlayed++;
-            }
-        }
-    }
-
-
+	override public function update(elapsed:Float):Void {
+		for (i in sequence) {
+			if ((i.timestamp + offset) < Conductor.songPosition && !i.played) {
+				i.played = true;
+				onLyricShow.dispatch(i);
+				elapsedLines.push(unplayedLines.shift());
+				linesPlayed++;
+			}
+		}
+	}
 }
