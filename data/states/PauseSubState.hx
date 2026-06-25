0a29671679c6c4e5190852090fce5ae626a483cd
Claire Vivian de Lunette <whotookclaireee@gmail.com>
2026-06-25 21:38:20 +0800

    dont show the meta if the file is non-existent

--- data/states/PauseSubState.hx
+++ data/states/PauseSubState.hx
@@ -6,16 +6,20 @@ import source.LyricHandler;
 
 var lrc = new LyricHandler();
 var lemeta:FunkinText;
+
 function postCreate() {
-    lrc.parseFromSong(PlayState.SONG.meta.name, PlayState.difficulty);
-    
-    lemeta  = ClefUtils.makeText(32, 32, "meow", 16, "right", true);
+	lrc.parseFromSong(PlayState.SONG.meta.name, PlayState.difficulty);
 
-    lemeta.text =         lrc.extras["ti"] ?? "?";
-    lemeta.text += "\nby" + lrc.extras["ar"] ?? "?";
-    lemeta.text += "\nfrom" + lrc.extras["al"] ?? "?";
+	if (lrc.extras.length == 0)
+		disableScript();
 
-    lemeta.x = window.width - 16 - lemeta.width;
-    lemeta.y = window.height - 16 - lemeta.height;
-    add(lemeta);
+	lemeta = ClefUtils.makeText(32, 32, "meow", 16, "right", true);
+
+	lemeta.text = lrc.extras["ti"] ?? "?";
+	lemeta.text += "\nby" + lrc.extras["ar"] ?? "?";
+	lemeta.text += "\nfrom" + lrc.extras["al"] ?? "?";
+
+	lemeta.x = window.width - 16 - lemeta.width;
+	lemeta.y = window.height - 16 - lemeta.height;
+	add(lemeta);
 }
